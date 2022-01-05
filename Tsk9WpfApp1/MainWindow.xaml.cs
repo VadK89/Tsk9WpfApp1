3c1f7138d6668d24762b49e888d7a7f014569ba2
Вадим <Vadimk89@mail.ru>
2022-01-05 23:07:55 +0500

    Добавьте файлы проекта.

---
+++ Tsk9WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tsk9WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            stylesBox.ItemsSource = styles;
+            stylesBox.SelectionChanged += ThemeChanged;
+            stylesBox.SelectedIndex = 0;
+        }
+
+        private void ThemeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = stylesBox.SelectedIndex;
+            Uri uri = new Uri("Light.xaml", UriKind.Relative);//Переменная со ссылкой на словарь ресурсов
+            if (styleIndex == 1)
+            {
+                uri = new Uri("Dark.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текcтовые файлы (*.txt)|*.txt|Все файлы (*.*)|(*.*)";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextRange doc = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+                using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open))
+                {
+                    if (System.IO.Path.GetExtension(openFileDialog.FileName).ToLower() == ".txt")
+                        doc.Load(fs, DataFormats.Text);
+                    else
+                        doc.Load(fs, DataFormats.Xaml);
+                }
+            }
+
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текcтовые файлы (*.txt)|*.txt|Все файлы (*.*)|(*.*)";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TextRange doc = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+                using (FileStream fs = File.Create(saveFileDialog.FileName))
+                {
+                    if (System.IO.Path.GetExtension(saveFileDialog.FileName).ToLower() == ".txt")
+                        doc.Save(fs, DataFormats.Text);
+                    else
+                        doc.Save(fs, DataFormats.Xaml);
+                }
+            }
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        //Из изначальной задачи без изменений
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = (sender as ComboBox).SelectedItem as string;
+            if (textBox != null)
+            {
+                textBox.FontSize = Convert.ToDouble(fontSize);
+            }
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            //RadioButton radioButton = RadioButton as sender;
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+    }
+}
